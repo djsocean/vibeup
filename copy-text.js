8a4a3373e537bd5cff496559c5d742df327a3b91
DJs Ocean <93548430+djsocean@users.noreply.github.com>
2023-03-07 18:54:42 -0500

    Create copy-text.js

---
+++ copy-text.js
@@ -0,0 +1,8 @@
+<script>
+// Get the text block and input box elements
+var textBlock = document.getElementById("header-statement-text");
+var inputBox = document.getElementById("header-statement-input");
+
+// Copy the text from the text block to the input box on page load
+inputBox.value = textBlock.innerText;
+</script>
